57c8b02fb3eb09fbe195fe4206c3cb7a0d524a9b
voldenet <voldenet@gmail.com>
2017-05-03 11:25:52 +0200

    masscmd.pl: change the foreach order arguments to more foreachy

--- mass_cmd.pl
+++ mass_cmd.pl
@@ -1,36 +1,36 @@
-weechat::register("mass_cmd", "voldenet", "0.1", "GPL3", "masskick", "", "");
-weechat::hook_command("masscmd", "", "", "", "", "masscmd", "");
+weechat::register("mass_cmd", "voldenet", "0.1", "GPL3", "/mass_cmd /anything {nick}", "", "");
+weechat::hook_command("mass_cmd", "", "", "", "", "mass_cmd", "");
 
-sub foreach_infolist(&$$$) {
-   my($sub, $name, $ptr, $args) = @_;
+sub foreach_infolist($$$&) {
+   my($name, $ptr, $args, $fn) = @_;
    my $list_ptr = weechat::infolist_get($name, $ptr, $args);
    while(weechat::infolist_next($list_ptr)) {
-      &{$sub}($list_ptr);
+      &{$fn}($list_ptr);
    }
    weechat::infolist_free($list_ptr);
 }
 
-sub foreach_nick(&$) {
-   my($sub, $buffer) = @_;
-   foreach_infolist(sub {
+sub foreach_nick($&) {
+   my($buffer, $fn) = @_;
+   foreach_infolist 'nicklist', $buffer, '', sub {
       return if weechat::infolist_integer($_[0], 'visible') == "0";
-      my $n = weechat::infolist_string($_[0], 'name');
-      return if $n =~ /^\d+\|/;
-      &{$sub}($n);
-   }, 'nicklist', $buffer, '');
+      my $nick = weechat::infolist_string($_[0], 'name');
+      return if $nick =~ /^\d+\|/;
+      &{$fn}($nick);
+   };
 }
 
 sub list_nicks($) {
    my $buffer = shift;
    my @names;
-   foreach_nick(sub {
+   foreach_nick($buffer, sub {
       my $name = shift;
       push(@names, $name);
-   }, $buffer);
+   });
    return \@names;
 }
 
-sub masscmd {
+sub mass_cmd {
    my($data, $buffer, $args) = @_;
    my $me = weechat::buffer_get_string($buffer, 'localvar_nick');
    my @nicks_to_kick = grep { $_ ne $me } reverse(@{list_nicks $buffer});
